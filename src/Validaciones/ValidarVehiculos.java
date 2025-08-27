3183fc467e53e6faf395ae92f240b16fa9ac7d58
Brandon-Valdelomar <brvaldelomarco@est.utn.ac.cr>
2025-08-26 23:18:51 -0600

    Error en nombres de variables, corregido.
    Error en nombre de variables, corregido.

--- src/Validaciones/ValidarVehiculos.java
+++ src/Validaciones/ValidarVehiculos.java
@@ -4,7 +4,6 @@ package Validaciones;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Validaciones;
 
 
 import Entidades.EstadoVehiculos;
