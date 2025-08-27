5728db84f8c23612b9623bf139a59963db77f437
Brandon-Valdelomar <brvaldelomarco@est.utn.ac.cr>
2025-08-26 22:40:58 -0600

    ValidarVehiculo
    Se movio de lugar Almacenar Vehiculo en su paquete correspondiente.

--- src/Validaciones/ValidarVehiculos.java
+++ src/Validaciones/ValidarVehiculos.java
@@ -1,7 +1,10 @@
+package Validaciones;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 
 import Entidades.EstadoVehiculos;
 import Entidades.TipoVehiculo;
