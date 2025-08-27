3ce723edffc5bdf204a5ff920028d84e6c117a94
samus <samuelcedas19@gmail.com>
2025-08-27 00:07:16 -0600

    ExcepcionesClientes
    Se implementaron las excepciones de clientes

---
+++ src/Excepciones/ClientesExcepciones/ClienteLicenciaRequerida.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Excepciones.ClientesExcepciones;
+
+/**
+ *
+ * @author samue
+ */
+public class ClienteLicenciaRequerida extends Exception {
+
+    public ClienteLicenciaRequerida() {
+        super();
+    }
+    
+}
--- src/Excepciones/ClientesExcepciones/ClienteMenorEdad.java
+++ src/Excepciones/ClientesExcepciones/ClienteMenorEdad.java
@@ -2,12 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Excepciones.ClienteExcepciones;
+package Excepciones.ClientesExcepciones;
 
 /**
  *
- * @author Eduard Salas Murillo
+ * @author samue
  */
-public class NewClass {
+public class ClienteMenorEdad extends Exception {
+
+    public ClienteMenorEdad() {
+        super();
+    }
     
 }
