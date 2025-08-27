c1334773d81589cea3ef4eaad0c5b248fa9c09cf
joespinoza11 <joespinozapa@est.utn.ac.cr>
2025-08-27 17:46:12 -0600

    Clases FrmMenú y DesktopPaneConFondo
    Se modificaron para poder sincronizar los internalFrames

--- src/GUI/DesktopConFondo.java
+++ src/GUI/DesktopConFondo.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JInternalFrame.java to edit this template
+ */
 package GUI;
 
 import javax.swing.*;
