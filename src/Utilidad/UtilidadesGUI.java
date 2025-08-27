0644568ae542bd705568e4d96f91e0a6056c4a3d
joespinoza11 <joespinozapa@est.utn.ac.cr>
2025-08-27 01:17:28 -0600

    Clase UtilidadesGUI
    Se creo para mostrar mensajes al momento de lanzar excepciones y otras funciones para la GUI

--- src/Utilidad/UtilidadesGUI.java
+++ src/Utilidad/UtilidadesGUI.java
@@ -4,10 +4,56 @@
  */
 package Utilidad;
 
+import java.awt.Component;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JOptionPane;
+import javax.swing.JSpinner;
+import javax.swing.text.JTextComponent;
+
 /**
  *
- * @author Eduard Salas Murillo
+ * @author samue
  */
-public class NewClass {
+public class UtilidadesGUI {
+    private static boolean tieneValor(JComponent txt) {
+        switch (txt) {
+            case JTextComponent text -> {
+                String s = text.getText();
+                return s != null && !s.trim().isEmpty();
+            }
+            case JComboBox<?> combo -> {
+                Object i = combo.isEditable() ? combo.getEditor().getItem() : combo.getSelectedItem();
+                return i != null && !i.toString().trim().isEmpty();
+            }
+            case JSpinner sp -> {
+                Object v = sp.getValue();
+                return v != null && !String.valueOf(v).trim().isEmpty();
+            }
+            default -> {
+                return true;
+            }
+        }
+    }
     
+    public static boolean validarRequiere(JComponent... txts) {
+        for (JComponent txt : txts) {
+            if (!tieneValor(txt)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private static void mostrarMensaje(Component component, Object message,String title,int messageType) {
+        JOptionPane.showMessageDialog(component,message, title,messageType);
+    }
+    
+    public static void mostrarMensaje(Component component, Object message,String title) {
+        mostrarMensaje(component, message, title,JOptionPane.INFORMATION_MESSAGE);
+    }
+    
+    public static void mostrarMensajeDeError(Component component, Object message,String title) {
+        mostrarMensaje(component, message, title,JOptionPane.ERROR_MESSAGE);
+    }
 }
