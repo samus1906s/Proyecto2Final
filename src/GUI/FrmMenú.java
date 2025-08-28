c67f70715ab48992bd195c13f926a1db0c9103a9
samus <samuelcedas19@gmail.com>
2025-08-28 03:58:11 -0600

    Correción de clientes

--- src/GUI/FrmMenú.java
+++ src/GUI/FrmMenú.java
@@ -140,7 +140,7 @@ public class FrmMenú extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void GestorClientesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_GestorClientesActionPerformed
-        abrirInternal(new IntFrmClientes());
+       
     }//GEN-LAST:event_GestorClientesActionPerformed
 
     private void GestorAlquileresActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_GestorAlquileresActionPerformed
